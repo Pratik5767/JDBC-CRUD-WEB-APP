0d116235204c2f46596afebaa2ebfab31a39e33f
Pratik5676 <pratikvsalunkhe924@gmail.com>
2025-02-03 21:51:33 +0530

    Update utils

--- JDBCCRUDWEBAPP/src/main/java/com/utils/JdbcUtil.java
+++ JDBCCRUDWEBAPP/src/main/java/com/utils/JdbcUtil.java
@@ -9,6 +9,9 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Properties;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
 public class JdbcUtil {
 	// so that no object should be created
 	private JdbcUtil() {
@@ -25,27 +28,36 @@ public class JdbcUtil {
 
 	// Establish the connection
 	public static Connection getJdbcConnection() throws SQLException, IOException {
-		FileInputStream fis = new FileInputStream(
-				"D:\\Pratik\\git\\JDBC-CRUD-WEB-APP\\JDBCCRUDWEBAPP\\src\\main\\java\\com\\properties\\application.properties");
+		/*
+		 * String fileLoc =
+		 * "D:\\Pratik\\git\\JDBC-CRUD-WEB-APP\\JDBCCRUDWEBAPP\\src\\main\\java\\com\\properties\\application.properties";
+		 * HikariConfig config = new HikariConfig(fileLoc); HikariDataSource dataSource
+		 * = new HikariDataSource(config); return dataSource.getConnection();
+		 */
+		
+		return physicalConnection();
+	}
+
+	public static Connection physicalConnection() throws SQLException, IOException {
+		FileInputStream fis = new FileInputStream("D:\\Pratik\\git\\JDBC-CRUD-WEB-APP\\JDBCCRUDWEBAPP\\src\\main\\java\\com\\properties\\application.properties");
 		Properties properties = new Properties();
 		properties.load(fis);
 
 		Connection connection = DriverManager.getConnection(properties.getProperty("jdbcUrl"),
 				properties.getProperty("user"), properties.getProperty("password"));
 		return connection;
-
 	}
 
 	public static void closeConnection(Connection connection, PreparedStatement preparedStatement, ResultSet resultSet)
 			throws SQLException {
-		if (connection != null) {
-			connection.close();
+		if (resultSet != null) {
+			resultSet.close();
 		}
 		if (preparedStatement != null) {
 			preparedStatement.close();
 		}
-		if (resultSet != null) {
-			resultSet.close();
+		if (connection != null) {
+			connection.close();
 		}
 	}
 }
